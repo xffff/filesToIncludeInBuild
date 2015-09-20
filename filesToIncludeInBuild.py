6b92a58d6c1a1b90a9742571041b67473d3f6ae3
unknown <MMURPHY@LUK-PC024WTR.corp.capgemini.com>
2015-09-20 11:33:13 +0100

    first commit, create remote repo

---
+++ filesToIncludeInBuild.py
@@ -0,0 +1,24 @@
+# filesToIncludeInBuild.py
+# michael murphy 2015
+# 
+# given a package.xml file, remove all files not referenced
+# in the file
+
+from BeautifulSoup import BeautifulSoup
+
+typeSuffixMap = {}
+
+class RemoveFiles:
+    def __init__(self):
+        ''' init class '''
+        pass
+
+    def removeFiles(self, pathList):
+        ''' given a list of filePaths, remove the files '''
+        pass
+
+class GetFiles:
+    def __init__(self):
+        pass
+
+    
