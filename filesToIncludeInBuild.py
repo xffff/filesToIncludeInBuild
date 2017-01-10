aa3d99b9fee0ee7081c548595f243e513ea78a5c
Michael Murphy <m.j.murphy@pwc.com>
2017-01-10 19:24:01 +0000

    classify this

--- filesToIncludeInBuild.py
+++ filesToIncludeInBuild.py
@@ -1,192 +1,190 @@
 ##################################
 # filesToIncludeInBuild.py
+#
 # michael murphy 2015
+#
 ##################################
+#
 # delete all files that are not 
-# referenced in package.xml
+# referenced in a supplied
+# package.xml
+#
 ##################################
 
 from lxml import etree
 import sys, os, re, argparse, shutil
 
-_debug = False
-_delete = False
+class FilesToIncludeInBuild():
 
-def parseXml(rootdir, packagexml, configxml):
-    ''' parse the XML file '''
-    global _debug
+    def __init__(self, _debug, _delete):
+        pass
+
+    def parseXml(self, rootdir, packagexml, configxml):
+        ''' parse the XML files
+        try to create three dictionaries describing
+        the way the data between the xml files is related '''
     
-    if _debug:
-        print("parsing: {0}".format(packagexml))
-
-    typememberdict = {}
-    typefileextdict = {}
-    typefilepathdict = {}
-
-    root = etree.XML(removeNamespaceReturnRoot(packagexml))
-    packageroot = etree.XML(removeNamespaceReturnRoot(packagexml))
-    configroot = etree.XML(removeNamespaceReturnRoot(configxml))
-    
-    # create a dict of name->members
-    for i in packageroot.iter("types"):
-        # get the name tag
-        name = i.find("name").text
-        members = [x.text for x in i.findall("members")]
-
         if _debug:
-            print("typememberdict[{0}] = {1}".format(name, members))
+            print("parsing: {0}".format(packagexml))
 
-        typememberdict[name] = members
+        typememberdict = {}
+        typefileextdict = {}
+        typefilepathdict = {}
 
-    # look up the file extension (potentially can be omitted)
-    # and the relative file path for the current type name
-    # from the XML config file specified in the argument...
-    for i in configroot.iter("types"):
-        # get the tag for the file extension
-        try:
+        packageroot = etree.XML(removeNamespaceReturnRoot(packagexml))
+        configroot = etree.XML(removeNamespaceReturnRoot(configxml))
+    
+        # create a dict of name->members
+        for i in packageroot.iter("types"):
+            # get the name tag
             name = i.find("name").text
-            extension = i.find("extension").text
-            folder = i.find("folder").text
-            
-            typefilepathdict[name] = os.path.join(rootdir, folder)
-            typefileextdict[name] = extension
+            members = [x.text for x in i.findall("members")]
 
             if _debug:
-                print("Join paths: {0} + {1} = {2}"   \
-                      .format(rootdir, folder, os.path.join(rootdir, folder)))
-                print("File extension for {0} is {1}" \
-                      .format(name, extension))
-        except Exception as e:
-            sys.exit("Error reading file: {0}".format(e.args))
-    
-    #for each type get contents of folder as map
-    typefoldercontentsdict = getFolderContents(typememberdict
-                                               , typefilepathdict)
+                print("typememberdict[{0}] = {1}".format(name, members))
 
-        
-    if _debug:
-        print("typefilepathdict: {0}"       \
-              .format(typefilepathdict.items()))
-        print("typefoldercontentsdict: {0}" \
-              .format(typefoldercontentsdict.items()))
-        print("typememberdict: {0}"	    \
-              .format(typememberdict.items()))
-        print("typefileextdict: {0}"	    \
-              .format(typefileextdict.items()))
+            typememberdict[name] = members
 
-    # delete the ones that do not match
-    removeFiles(typefoldercontentsdict, typememberdict, typefilepathdict, typefileextdict)
+        # look up the file extension (potentially can be omitted)
+        # and the relative file path for the current type name
+        # from the XML config file specified in the argument...
+        for i in configroot.iter("types"):
+            # get the tag for the file extension
+            try:
+                name = i.find("name").text
+                extension = i.find("extension").text
+                folder = i.find("folder").text
 
-def getFolderContents(typememberdict, typefilepathdict):
-    ''' using the typememberdict keyset as a hook
-        for each type, get the contents of the folder
-        specified for this type by the typefilepathdict '''
-    global _debug
-    
-    typefoldercontentsdict = {}
-    
-    for k, v in typememberdict.items():
-        try:
-            filepath = typefilepathdict[k]
-            if "*" not in filepath:
+                typefilepathdict[name] = os.path.join(rootdir, folder)
+                typefileextdict[name] = extension
+
                 if _debug:
-                    print("filepath: {0}".format(filepath))
-                    typefoldercontentsdict[k] = os.listdir(filepath)
-        except KeyError as e:
-            ''' we could save these and use them later
-                probably for something else like knowing
-                that they can be deleted... hooray '''
-            print("No key: {0} for type member dict".format(e.args))
-
-    return typefoldercontentsdict
+                    print("Join paths: {0} + {1} = {2}"   \
+                          .format(rootdir, folder, os.path.join(rootdir, folder)))
+                    print("File extension for {0} is {1}" \
+                          .format(name, extension))
+            except Exception as e:
+                sys.exit("Error reading file: {0}".format(e.args))
     
-
-def removeNamespaceReturnRoot(packagexml):
-    ''' read to string and remove the xmlns 
-        because it just causes loads of issues '''
-
-    xmldata = ""
-
-    with open(packagexml, "r") as myfile:
-        xmldata = myfile.read()
-
-    xmldata = re.sub('xmlns=".*"', '', xmldata, count=1)
-    xmldata = re.sub('<\?.*\?>', '', xmldata, count=1)
-
-    return xmldata
+        #for each type get contents of folder as map
+        typefoldercontentsdict = getFolderContents(typememberdict
+                                                   , typefilepathdict)
 
 
-def removeFiles(typefoldercontentsdict, typememberdict, typefilepathdict, typefileextdict):
-    ''' remove files from filesystem 
-        first check for each key whether the value list is the same size
-        if it isnt then sort both value lists so they are ordered sets
-        then iterate through and compare each value
-        if a != b then delete b from the filesystem '''
-    global _debug
-    global _delete
-
-    for k, v in typefilepathdict.items():
         if _debug:
-            print("key: {0}, value: {1}".format(k, v))
+            print("typefilepathdict: {0}"       \
+                  .format(typefilepathdict.items()))
+            print("typefoldercontentsdict: {0}" \
+                  .format(typefoldercontentsdict.items()))
+            print("typememberdict: {0}"	    \
+                  .format(typememberdict.items()))
+            print("typefileextdict: {0}"	    \
+                  .format(typefileextdict.items()))
 
-        if k not in typememberdict.keys():
+        # delete the ones that do not match
+        removeFiles(typefoldercontentsdict, typememberdict, typefilepathdict, typefileextdict)
+
+    def getFolderContents(self, typememberdict, typefilepathdict):
+        ''' using the typememberdict keyset as a hook
+            for each type, get the contents of the folder
+            specified for this type by the typefilepathdict '''
+
+        typefoldercontentsdict = {}
+
+        for k, v in typememberdict.items():
+            try:
+                filepath = typefilepathdict[k]
+                if "*" not in filepath:
+                    if _debug:
+                        print("filepath: {0}".format(filepath))
+                        typefoldercontentsdict[k] = os.listdir(filepath)
+            except KeyError as e:
+                ''' we could save these and use them later
+                    probably for something else like knowing
+                    that they can be deleted... hooray '''
+                print("No key: {0} for type member dict".format(e.args))
+
+        return typefoldercontentsdict
+    
+
+    def removeNamespaceReturnRoot(self, packagexml):
+        ''' read to string and remove the xmlns 
+            because it just causes loads of issues '''
+
+        xmldata = ""
+
+        with open(packagexml, "r") as myfile:
+            xmldata = myfile.read()
+
+        xmldata = re.sub('xmlns=".*"', '', xmldata, count=1)
+        xmldata = re.sub('<\?.*\?>', '', xmldata, count=1)
+
+        return xmldata
+
+
+    def removeFiles(self, typefoldercontentsdict, typememberdict, typefilepathdict, typefileextdict):
+        ''' remove files from filesystem 
+            first check for each key whether the value list is the same size
+            if it isnt then sort both value lists so they are ordered sets
+            then iterate through and compare each value
+            if a != b then delete b from the filesystem '''
+
+        for k, v in typefilepathdict.items():
             if _debug:
-                print("Removing key: {0} as it was not in {1}".format(k, typememberdict.keys()))
+                print("key: {0}, value: {1}".format(k, v))
 
-            # delete all files in the directory
-            if _delete:
-                try:
-                    if os.path.exists(typefilepathdict[k]):
-                        shutil.rmtree(typefilepathdict[k])
-                except Exception  as e:
-                    print("Could not delete {0} the exception was: {1}".format(typefilepathdict[k], e.args))
-        else:
-            # if the members contain the element * 
-            # then we want to keep everything dont we
-            if "*" not in typememberdict[k]:
-                # otherwise sort the lists
-                typefolderlist = typefoldercontentsdict[k]                
-                typememberlist = [i + "." + typefileextdict[k] for i in typememberdict[k]]
-                typemetalist = [i + "-meta.xml" for i in typememberlist]
-                
-                typefolderlist.sort()
-                typememberlist.sort()
-
+            if k not in typememberdict.keys():
                 if _debug:
-                    print('''typefolderlist: {0}
-                    	     \ntypememberlist: {1}
-                    	     \ntypemetalist: {2}''' \
-                          .format(typefolderlist
-                                  , typememberlist
-                                  , typemetalist))
+                    print("Removing key: {0} as it was not in {1}".format(k, typememberdict.keys()))
 
-                print("typefolderlist: {0}, typememberlist: {1}" \
-                      .format(typefolderlist, typememberlist))
+                # delete all files in the directory
+                if _delete:
+                    try:
+                        if os.path.exists(typefilepathdict[k]):
+                            shutil.rmtree(typefilepathdict[k])
+                    except Exception  as e:
+                        print("Could not delete {0} the exception was: {1}".format(typefilepathdict[k], e.args))
+            else:
+                # if the members contain the element * 
+                # then we want to keep everything dont we
+                if "*" not in typememberdict[k]:
+                    # otherwise sort the lists
+                    typefolderlist = typefoldercontentsdict[k]                
+                    typememberlist = [i + "." + typefileextdict[k] for i in typememberdict[k]]
+                    typemetalist = [i + "-meta.xml" for i in typememberlist]
 
-                # if a file is not in the package xml but
-                # is in the filesystem, it should be deleted
-                # so it does not appear in the build
-                for i in typefolderlist:
-                    if i not in typememberlist and i not in typemetalist:
-                        try:
-                            print("deleting: {0}/{1}".format(typefilepathdict[k], i))
+                    typefolderlist.sort()
+                    typememberlist.sort()
 
-                            # actually do the delete
-                            if _delete:
-                                os.remove("{0}/{1}".format(typefilepathdict[k], i))
-                        except Exception as e:
-                            sys.exit("typefilepathdict exception: {0}".format(e.args))
+                    if _debug:
+                        print('''typefolderlist: {0}
+                                 \ntypememberlist: {1}
+                                 \ntypemetalist: {2}''' \
+                              .format(typefolderlist
+                                      , typememberlist
+                                      , typemetalist))
+
+                    print("typefolderlist: {0}, typememberlist: {1}" \
+                          .format(typefolderlist, typememberlist))
+
+                    # if a file is not in the package xml but
+                    # is in the filesystem, it should be deleted
+                    # so it does not appear in the build
+                    for i in typefolderlist:
+                        if i not in typememberlist and i not in typemetalist:
+                            try:
+                                print("deleting: {0}/{1}".format(typefilepathdict[k], i))
+
+                                # actually do the delete
+                                if _delete:
+                                    os.remove("{0}/{1}".format(typefilepathdict[k], i))
+                            except Exception as e:
+                                sys.exit("typefilepathdict exception: {0}".format(e.args))
                             
-
-def usage():
-    ''' print usage info to command line '''
-    print("usage")
 
 def filesToIncludeInBuild(argv):
     ''' main function, get args and start doing stuff '''
-    global _debug
-    global _delete
-
+    _debug, _delete = False
     packagexml = None
     configxml = None
     rootdir = None
@@ -226,11 +224,13 @@ def filesToIncludeInBuild(argv):
     if not os.path.exists(args.config):
         sys.exit("the config file doesnt exist")
     if not os.path.exists(args.root):
-        sys.exit("the root folder doesnt exist")
+        sys.exit("the src folder doesnt exist")
 
-    parseXml(os.path.realpath(args.root)
-             , os.path.realpath(args.package)
-             , os.path.realpath(args.config))
+    ftib = FilesToIncludeInBuild(_debug ,_default)
+
+    ftib.parseXml(os.path.realpath(args.root)
+                  , os.path.realpath(args.package)
+                  , os.path.realpath(args.config))
 
         
 if __name__ == "__main__":
